28bbcb64d025a9a150f5a99f2d7e8654f8ef9a65
mickeysrishti <Mukeshpathak345@gmail.com>
2023-03-23 03:12:46 +0530

    Post Featured Desigend

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,14 @@
+import "./post.css";
+
+export default function Post() {
+  return (
+    <div className="Post">
+        <div className="postWrapper">
+            <div className="postTop"></div>
+            <div className="postCenter"></div>
+            <div className="postBottom"></div>
+        </div>
+    
+    </div>
+  )
+}
--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,4 +1,4 @@
-import { PermMedia } from "@mui/icons-material";
+import { PermMedia, Label, Room, EmojiEmotions } from "@mui/icons-material";
 import "./share.css";
 
 
@@ -14,13 +14,26 @@ export default function Share() {
             <hr className="shareHr" />
             <div className="shareBottom">
                 <div className="shareOptions">
-                <div className="shareOption">
-                <PermMedia className="shareIcon" />
-                    <span className="shareOptionText">Photo or Video</span>
-
+                     <div className="shareOption">
+                         <PermMedia htmlColor="tomato" className="shareIcon" />
+                             <span className="shareOptionText">Photo or Video</span>
+                     </div>
+                     <div className="shareOption">
+                         <Label htmlColor="blue" className="shareIcon" />
+                             <span className="shareOptionText">Tag</span>
+                     </div>
+                     <div className="shareOption">
+                         <Room htmlColor="green" className="shareIcon" />
+                             <span className="shareOptionText">Location</span>
+                     </div>
+                     <div className="shareOption">
+                         <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+                             <span className="shareOptionText">Feelings</span>
+                     </div>
                 </div>
-
-                </div>
+                <button className="shareButton">
+                    Share
+                </button>
 
             </div>
         </div>
